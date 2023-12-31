43d8c6bb45f37d153a50f7e75a53fed6c24c46fc
Marco Vermeulen <vermeulen.mp@gmail.com>
2023-12-31 13:14:14 +0000

    Introduce CandidateVersion

---
+++ src/main/kotlin/sdkman/io/plugins/Domain.kt
@@ -0,0 +1,17 @@
+package sdkman.io.plugins
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CandidateVersion(
+    val candidate: String,
+    val version: String,
+    val platform: String,
+    val url: String,
+    val visible: Boolean,
+    val vendor: String?,
+    val md5sum: String?,
+    val sha256sum: String?,
+    val sha512sum: String?
+)
+
