b2ff0159fbd2f76f2616993da587f134af4ef1c0
Marco Vermeulen <vermeulen.mp@gmail.com>
2023-12-31 13:21:31 +0000

    Introduce CandidateVersionsRepository

---
+++ src/main/kotlin/sdkman/io/plugins/CandidateVersionsRepository.kt
@@ -0,0 +1,41 @@
+package sdkman.io.plugins
+
+import kotlinx.coroutines.Dispatchers
+import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+
+class CandidateVersionsRepository {
+    object CandidateVersions : IntIdTable(name = "version") {
+        val candidate = varchar("candidate", length = 20)
+        val version = varchar("version", length = 25)
+        val platform = varchar("platform", length = 15)
+        val visible = bool("visible")
+        val url = varchar("url", length = 500)
+        val vendor = varchar("vendor", length = 10).nullable()
+        val md5Sum = varchar("md5_sum", length = 32).nullable()
+        val sha256Sum = varchar("sha_256_sum", length = 64).nullable()
+        val sha512Sum = varchar("sha_512_sum", length = 128).nullable()
+    }
+
+    private suspend fun <T> dbQuery(block: suspend () -> T): T =
+        newSuspendedTransaction(Dispatchers.IO) { block() }
+
+    suspend fun read(candidate: String): List<CandidateVersion> =
+        dbQuery {
+            CandidateVersions.select { CandidateVersions.candidate eq candidate }
+                .map {
+                    CandidateVersion(
+                        candidate = it[CandidateVersions.candidate],
+                        version = it[CandidateVersions.version],
+                        platform = it[CandidateVersions.platform],
+                        url = it[CandidateVersions.url],
+                        vendor = it[CandidateVersions.vendor],
+                        visible = it[CandidateVersions.visible],
+                        md5sum = it[CandidateVersions.md5Sum],
+                        sha256sum = it[CandidateVersions.sha256Sum],
+                        sha512sum = it[CandidateVersions.sha512Sum]
+                    )
+                }
+        }
+}
