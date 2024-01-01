4f377b58fa4b73af12c465c0aef8abea7b3a9448
Marco Vermeulen <vermeulen.mp@gmail.com>
2024-01-01 11:25:38 +0000

    Configure ignore null fields on JSON serialization

--- src/main/kotlin/sdkman/io/plugins/Serialization.kt
+++ src/main/kotlin/sdkman/io/plugins/Serialization.kt
@@ -3,9 +3,12 @@ package sdkman.io.plugins
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
 
+@OptIn(ExperimentalSerializationApi::class)
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
-        json()
+        json( Json { explicitNulls = false })
     }
 }
