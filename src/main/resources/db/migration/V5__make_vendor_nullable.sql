d2ac855fd4ee04fb58b2588c9efe676cb56502a5
Marco Vermeulen <vermeulen.mp@gmail.com>
2025-08-24 14:19:54 +0100

    docs: add todo for recreating schema

--- src/main/resources/db/migration/V5__make_vendor_nullable.sql
+++ src/main/resources/db/migration/V5__make_vendor_nullable.sql
@@ -1,3 +1,4 @@
+-- TODO: The table doesn't hold any valuable data yet. Instead, drop the entire table and re-create with new schema
 -- Make vendor column nullable and clear existing data as specified
 DELETE FROM versions;
 ALTER TABLE versions ALTER COLUMN vendor DROP NOT NULL;
